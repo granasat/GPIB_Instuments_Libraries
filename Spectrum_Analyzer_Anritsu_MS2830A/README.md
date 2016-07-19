47565f0971486581f6edb6ff87fe4b5cc8b89acf
Prof. Andres Roldán Aranda <amroldan@gmail.com>
2016-07-19 14:43:56 +0200

    Upload

---
+++ Spectrum_Analyzer_Anritsu_MS2830A/README.md
@@ -0,0 +1,13 @@
+#![alt text](https://granasat.ugr.es/templates/sj_plus/favicon.ico "GranaSAT Logo") Spectrum Analyzer Anritsu MS2830A 
+
+  This example files have been tested on GranaSAT GPIB Transceiver.
+
+  This two MATLAB files are the examples that the Students of the UGR have generated to remote control the Spectrum Analyzer Anritsu MS2830A [Link to the PDF Manual](http://electronica.ugr.es/%7Ejmolinos/ci_files/MS2830A.pdf)
+  The remote control protocol is documented in [Link to the PDF Manual](https://dl.cdn-anritsu.com/en-au/test-measurement/files/Manuals/Operation-Manual/remote_opm_e_32_0.pdf)
+  
+    
+  ---
+  
+  Front side of the Spectrum Analyzer: 
+![alt text](https://gwdata.cdn-anritsu.com/images/products/tm-ms2830a/ms2830a-spectrumanalyzer-signalanalyzer.png?h=310&w=420 "Image")
+  
---
+++ Spectrum_Analyzer_Anritsu_MS2830A/testLibAnritsu_MS2830A.m
@@ -0,0 +1,37 @@
+clc; clear all;
+%fclose(instrfind);
+
+instrument.name = 'Marconi-2022D';
+instrument.serialPort = 'COM6';
+%Marconi_2022D_V01(instrument,'help',0); 
+
+try
+    Marconi_2022D_V01(instrument,'open',0)
+    
+    %Set carrier frequency characteristics:
+    frequency.value=200;
+    frequency.units='MHz';
+    frequency.modulator='internal';    
+    Marconi_2022D_V01(instrument,'set_frequency',frequency); 
+
+    %Set modulation characteristics:
+    modulation.type='FM';
+    modulation.value=20;
+    modulation.units='KHz';
+    modulation.oscillator='on';
+    modulation.modulator='internal';
+    modulation.ALC='on';  
+    Marconi_2022D_V01(instrument,'set_modulation',modulation); 
+    
+    %Set RF level characteristics:
+    rflevel.value=1.5;
+    rflevel.units='mV';
+    rflevel.carrier='on';
+    Marconi_2022D_V01(instrument,'set_rflevel',rflevel); 
+       
+    Marconi_2022D_V01(instrument,'close',0)
+catch err
+    Marconi_2022D_V01(instrument,'close',0)
+    fprintf('ERROR\n')
+    throw(err)
+end
